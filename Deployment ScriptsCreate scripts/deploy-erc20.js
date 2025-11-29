26966ff175ff60b00e4e80e6fd95fff334aa74f7
Richard Kindler <ricks.online.businesses425@gmail.com>
2025-11-29 10:29:15 -0700

    Create deploy-erc20.js
    Signed-off-by: Richard Kindler <ricks.online.businesses425@gmail.com>

---
+++ Deployment ScriptsCreate scripts/deploy-erc20.js
@@ -0,0 +1,17 @@
+const { ethers } = require('hardhat');
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying contracts with the account:', deployer.address);
+
+  const MyToken = await ethers.getContractFactory('MyToken');
+  const myToken = await MyToken.deploy();
+  
+  await myToken.waitForDeployment();
+  console.log('MyToken deployed to:', await myToken.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
